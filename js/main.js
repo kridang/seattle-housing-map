52a6dd8f34371645e9288538af04eb00392f9ee1
kridang <dangnikristy@gmail.com>
2025-12-05 00:20:14 -0800

    kridang: Added transit Nearest-Distance from Lightrails, Team Page, Styling

--- js/main.js
+++ js/main.js
@@ -8,19 +8,6 @@ function toggleNav() {
     }
 }
 
-// query popup
-// const helpBtn = document.getElementById("help-button");
-const popup = document.getElementById("popup");
-const tutorialClose = document.getElementById("tutorialClose");
-
-// helpBtn.addEventListener("click", () => {
-//     popup.classList.add("visible");
-// });
-
-tutorialClose.addEventListener("click", () => {
-    popup.classList.remove("visible");
-});
-
 // for maps
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3JpZGFuZyIsImEiOiJjbWhib3YwM3cxYmM0Mmxwdm00MmpubjdnIn0.A_2GT3PB5OEdbL9HLwlubQ'; // add token later
 const map = new mapboxgl.Map({
@@ -54,6 +41,81 @@ map.on('load', async () => {
 			map.setLayoutProperty("mha-fill", "visibility", newVis);
   });
 
+  // for bus
+  const transitResp = await fetch('data/transit.geojson');
+	const transit = await transitResp.json();
+
+	map.addSource('transit', { type: 'geojson', data: transit });
+
+	map.addLayer({
+		id: 'transit-layer',
+		type: 'circle',
+		source: 'transit',
+		paint: {
+			'circle-radius': 4,
+			'circle-color': '#919090',
+			'circle-stroke-width': 1,
+			'circle-stroke-color': 'white'
+		}
+	});
+
+	map.on("click", "transit-layer", async (e) => {
+    const busStop = e.features[0];
+    const busCoords = busStop.geometry.coordinates;
+    const busPt = turf.point(busCoords);
+
+    const nearest = turf.nearestPoint(busPt, lightrail);
+
+    if (!nearest) return;
+
+    const stationCoords = nearest.geometry.coordinates;
+
+    const url = `https://api.mapbox.com/directions/v5/mapbox/walking/` +
+        `${busCoords[0]},${busCoords[1]};${stationCoords[0]},${stationCoords[1]}` +
+        `?geometries=geojson&access_token=${mapboxgl.accessToken}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (!data.routes || data.routes.length === 0) return;
+
+        const route = data.routes[0].geometry;
+        const distanceKm = (data.routes[0].distance / 1000).toFixed(2);
+
+        map.getSource("lines-source").setData({
+            type: "FeatureCollection",
+            features: [
+                {
+                    type: "Feature",
+                    geometry: route
+                }
+            ]
+        });
+
+        new mapboxgl.Popup()
+            .setLngLat(busCoords)
+            .setHTML(`
+                <strong>Bus Stop ID:</strong> ${busStop.properties.STOP_ID}<br>
+                <strong>Nearest Lightrail:</strong> ${nearest.properties.NAME || "Station"}<br>
+                <strong>Walking Distance:</strong> ${distanceKm} km
+            `)
+            .addTo(map);
+
+        map.easeTo({ center: busCoords, zoom: 14 });
+
+    } catch (error) {
+        console.error("Walking route error:", error);
+    }
+	});
+
+	document.getElementById("busBtn").addEventListener("click", () => {
+    const current = map.getLayoutProperty("transit-layer", "visibility");
+    const newVisibility = current === "none" ? "visible" : "none";
+    map.setLayoutProperty("transit-layer", "visibility", newVisibility);
+		map.setLayoutProperty("lines-source", "visibility", newVisibility);
+	});
+
 	// for lightrail
 	const lightrailResp = await fetch('data/clean_lightrail.geojson');
 	const lightrail = await lightrailResp.json();
@@ -64,9 +126,9 @@ map.on('load', async () => {
 		type: 'circle',
 		source: 'lightrail',
 		paint: {
-				'circle-radius': 8,
-				'circle-color': 'blue',
-				'circle-stroke-width': 2,
+				'circle-radius': 7,
+				'circle-color': 'yellow',
+				'circle-stroke-width': 1,
 				'circle-stroke-color': 'white'
 		}
 	});
@@ -83,9 +145,9 @@ map.on('load', async () => {
     type: 'circle',
     source: 'uw-campus',
     paint: {
-			'circle-radius': 12,
+			'circle-radius': 8,
 			'circle-color': 'purple',
-			'circle-stroke-width': 2,
+			'circle-stroke-width': 1,
 			'circle-stroke-color': 'white'
     }
 	});
@@ -104,7 +166,6 @@ map.on('load', async () => {
     const start = station.geometry.coordinates;
     const end = [-122.303, 47.655]; // UW Campus PT!
 		
-		// From Turf
     const url = `https://api.mapbox.com/directions/v5/mapbox/walking/${start[0]},${start[1]};${end[0]},${end[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`;
 
     try {
@@ -139,7 +200,7 @@ map.on('load', async () => {
         const newVisibility = lightrail === "none" ? "visible" : "none";
 				const newUWVisibility = uw === "none" ? "visible" : "none";
         map.setLayoutProperty("lightrail-layer", "visibility", newVisibility);
-				map.setLayoutProperty("uw-campus-layer", "visibility", newUWVisibility);	
+				map.setLayoutProperty("uw-campus-layer", "visibility", newUWVisibility);
     });
 });
 
